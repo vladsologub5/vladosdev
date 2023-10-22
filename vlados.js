b909381b505c9d438824a0eeca0156c3a4696d2f
vlasologub5 <vladsologub5@gmail.com>
2023-10-23 02:47:55 +0300

    comment

---
+++ vlados.js
@@ -0,0 +1,30 @@
+const express = require('express')
+app = express()
+router = express.Router
+port = 3000
+
+let arr = ['GET method', 'POST method', 'PATCH method', 'DELETE method']
+
+router.get('/', (req, res) => {
+    arr.forEach(function(arr)  {
+        res.send(arr)
+    })
+})
+
+router.post('/', (req, res) => {
+    const { name } = req.body
+    arr.push(name)
+})
+
+router.patch('/', (req, res) => {
+    const { name } = req.body
+    arr.unshift(name)
+})
+
+router.delete('/', (req, res) => {
+    const { name } = req.body
+    arr.splice(name)
+})
+
+module.exports = router;
+app.listen(3000)
